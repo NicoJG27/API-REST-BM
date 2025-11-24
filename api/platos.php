3e81551698951d00068ef7061d10103df6917d8d
NicoJG27 <njimgut1905@g.educaand.es>
2025-11-24 18:48:26 +0100

    platos.php requieres

---
+++ api/platos.php
@@ -0,0 +1,11 @@
+<?php
+header("Content-Type: application/json; charset=utf-8");
+
+require_once __DIR__ . "/../config.php";
+require_once __DIR__ . "/../database.php";
+require_once __DIR__ . "/../modelo/plato_modelo.php";
+require_once __DIR__ . "/../controlador/platos_controlador.php";
+require_once __DIR__ . "/auth.php";
+
+
+?>
